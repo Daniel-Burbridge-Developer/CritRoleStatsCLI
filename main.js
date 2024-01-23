fa5f8e342437bffc147e778f15eaa3ac4f762c8a
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2024-01-23 21:40:09 +0800

    night one complete

--- main.js
+++ main.js
@@ -8,10 +8,3 @@ const critRolls = rollDistributor.getCritRolls();
 
 console.log(`There are ${allRolls.length} rolls in the data set.`);
 console.log(`There are ${critRolls.length} crits in the data set.`);
-
-
-// Cannot directly console.log the slice, unsure why - Just Javascript things I guess.
-// The Below code is working though as intended though, so I think it's good.
-firstTenCrits = critRolls.slice(0, 10);
-console.log(firstTenCrits);
-console.log(firstTenCrits[0]);
