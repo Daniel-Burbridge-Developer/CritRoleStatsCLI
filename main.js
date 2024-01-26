010a0c0230d7a6a362ce427433ee6ca777687856
Daniel-Burbridge-Developer <daniel.burbridge.developer@gmail.com>
2024-01-26 17:39:11 +0800

    It's actually broken now, but IDK progress?

--- main.js
+++ main.js
@@ -1,70 +1,81 @@
 import * as THREE from 'three';
-import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 // Can't get this to work with the import statement, so I'm using require for now. -- ES6????????
 //const { getRollDistributor } = require('./rollFactory');
 import { getRollDistributor } from './rollFactory';
 
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-const renderer = new THREE.WebGLRenderer();
-camera.position.z = 5;
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+const main = async () => {
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const renderer = new THREE.WebGLRenderer();
+    vmRollData = await fetchDataAndLoad('/data/Vox_Machina_Compaign_All_Rolls.csv');
+    camera.position.z = 5;
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
 
-// Load the model
-const loader = new GLTFLoader();
-loader.load('/dice/d20.glb', function (gltf) {
-    scene.add(gltf.scene);
-}, undefined, function (error) {
-    console.error(error);
-});
+    loadLights(scene);
 
-// The model I'm using needs a directional light - probably requires a fix
-const directionalLightTop = new THREE.DirectionalLight(0xffffff, 5);
-directionalLightTop.position.set(0, 10, 0);
+    const animate = () => {
+        requestAnimationFrame(animate);
+        spinAllDice();
+        renderer.render(scene, camera);
+    }
 
-const directionalLightLeft = new THREE.DirectionalLight(0xffffff, 5);
-directionalLightLeft.position.set(10, 0, 0);
+    const spinAllDice = () => {
+        scene.traverse(function (child) {
+            if (child.isMesh) {
+                child.rotation.x += 0.01;
+                child.rotation.y += 0.01;
+            }
+        });
+    }
 
-const directionalLightRight = new THREE.DirectionalLight(0xffffff, 5);
-directionalLightRight.position.set(-10, 0, 0);
+    animate();
 
-const directionalLightBottom = new THREE.DirectionalLight(0xffffff, 5);
-directionalLightBottom.position.set(0, -10, 0);
-
-const directionalLightFront = new THREE.DirectionalLight(0xffffff, 5);
-directionalLightFront.position.set(0, 0, 10);
-
-const directionalLightBack = new THREE.DirectionalLight(0xffffff, 5);
-directionalLightBack.position.set(0, 0, -10);
-
-scene.add(directionalLightTop);
-scene.add(directionalLightLeft);
-scene.add(directionalLightRight);
-
-// Render the scene
-function animate() {
-    requestAnimationFrame(animate);
-    renderer.render(scene, camera);
 }
 
-animate();
+const fetchDataAndLoad = async (filePath) => {
+    try {
+        const vmRollData = await getRollData(filePath);
+        console.log(vmRollData);
+        console.log("TESTING");
+        console.log(vmRollData[0])
+        console.log(vmRollData[0]["die"])
+    } catch(error) {
+        console.error("Error fetching file or loading data", error);
+        throw error;
+    }
+}
+
+const loadLights = (scene) => {
+    const directionalLightTop = new THREE.DirectionalLight(0xffffff, 2);
+    const directionalLightBottom = new THREE.DirectionalLight(0xffffff, 2);
+    const directionalLightLeft = new THREE.DirectionalLight(0xffffff, 2);
+    const directionalLightRight = new THREE.DirectionalLight(0xffffff, 2);
+    const directionalLightFront = new THREE.DirectionalLight(0xffffff, 2);
+    const directionalLightBack = new THREE.DirectionalLight(0xffffff, 2);
+
+    directionalLightTop.position.set(0, 10, 0);   
+    directionalLightLeft.position.set(10, 0, 0);
+    directionalLightRight.position.set(-10, 0, 0);
+    directionalLightBottom.position.set(0, -10, 0);
+    directionalLightFront.position.set(0, 0, 10);
+    directionalLightBack.position.set(0, 0, -10);
+
+    scene.add(directionalLightTop);
+    scene.add(directionalLightLeft);
+    scene.add(directionalLightRight); 
+    scene.add(directionalLightBottom);
+    scene.add(directionalLightFront);
+    scene.add(directionalLightBack);
+}
 
 // Placed in a function to keep it seperate from 3js code for now while I work it out.
-const rollData = () => {
-const vmRollDistributor = getRollDistributor('data/Vox_Machina_Campaign_All_Rolls.csv');
-const mnRollDistributor = getRollDistributor('data/Mighty_Nein_Campaign_All_Rolls.csv');
 
-const vmAllRolls = vmRollDistributor.getRolls();
-const vmCritRolls = vmRollDistributor.getCritRolls();
-
-const mnAllRolls = mnRollDistributor.getRolls();
-const mnCritRolls = mnRollDistributor.getCritRolls();
-
-console.log(`There are ${vmAllRolls.length} rolls in the VM data set.`);
-console.log(`There are ${vmCritRolls.length} crits in the VM data set.`);
-
-console.log(`There are ${mnAllRolls.length} rolls in the MN data set.`);
-console.log(`There are ${mnCritRolls.length} crits in the MN data set.`);
+const getRollData = async (filePath) => {
+    const RollDistributor = await getRollDistributor(filePath);
+    return RollDistributor.getRolls();
 }
+
+
+main();
--- rollFactory.js
+++ rollFactory.js
@@ -2,26 +2,56 @@
 // const fs = require('fs');
 // const Papa = require('papaparse');
 
-import fs from 'fs';
+// ES6 Breaks FS too..... I hate ES6 :)
+// import fs from 'fs';
 import Papa from 'papaparse';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
-export const getRollDistributor = (filePath) => {
-    const fileData = fs.readFileSync(filePath, 'utf8');
+export const getRollDistributor = async (filePath) => {
+    try {
+        const response = await fetch(filePath, {
+            headers: {
+                'Content-Type': 'text/plain; charset=utf-8',
+                'mode': 'no-cors'
+            }
+        });
+        
+        if (!response.ok) {
+            throw new Error('Failed to fetch file: ${response.statusText}')
+        }
+        const fileData = await response.text();
 
-    // Not entirely sure how destructuring works to be honest, But renaming the data variable to rollData is a weird format. 
-    const { data: rollData } = Papa.parse(fileData, { header: true });
-    return new RollDistributor(rollData);
-};
+        // Not entirely sure how destructuring works to be honest, But renaming the data variable to rollData is a weird format. 
+        const { data: rollData } = Papa.parse(fileData, { header: true });
+
+        return new RollDistributor(rollData);
+    } catch (error) {
+        console.error("Error fetching file or generating data", error);
+        throw error;
+    }
+}
 
 class RollDistributor {
     constructor(rollData) {
-        this.rolls = this.mapRolls(rollData);
+        this.rolls = null
+        this.init();
     }
 
-    mapRolls(rollData) {
-        return rollData.map((row) => { 
+    async init() {
+        try {
+            // Wait for the createDie promise to resolve
+            this.rolls = await this.mapRolls(rollData);
+        } catch (error) {
+            console.error('Error creating die:', error);
+        }
+    }
+
+    async mapRolls(rollData) {
+        const rollPromises = rollData.map((row) => {
             return new Roll(row['RollID'], row['Campaign'], row['Episode'], row['Time'], row['Player'], row['Character'], row['Type of Roll'], row['D20?'], row['Total Roll'], row['Natural Roll'], row['Crit?'], row['Notes']);
         });
+    
+        return Promise.all(rollPromises);
     }
 
     getRolls() {
@@ -35,7 +65,7 @@ class RollDistributor {
 
 class Roll {
     constructor(rollID, campaign, episode, time, player, character, typeOfRoll, d20, totalRoll, naturalRoll, crit, notes) {
-        this.rollID = parseInt(rollID),
+        this.rollID = parseInt(rollID);
         this.campaign = campaign;
         this.episode = parseInt(episode);
         this.time = time;
@@ -43,14 +73,38 @@ class Roll {
         this.character = character;
         this.type = typeOfRoll;
         this.d20 = d20 === 'Yes';
-        this.totalRoll = parseInt(totalRoll) ? parseInt(totalRoll) : 0,
-        this.naturalRoll = parseInt(naturalRoll) ? parseInt(naturalRoll) : 0,
+        this.totalRoll = parseInt(totalRoll) ? parseInt(totalRoll) : 0;
+        this.naturalRoll = parseInt(naturalRoll) ? parseInt(naturalRoll) : 0;
         this.crit = crit === 'Yes';
         this.notes = notes;
+        this.die = null; // Initialize to null
+
+        // Going Async
+        this.init();
+    }
+
+    async init() {
+        try {
+            // Wait for the createDie promise to resolve
+            this.die = await this.createDie();
+        } catch (error) {
+            console.error('Error creating die:', error);
+        }
     }
 
     isCrit() {
         return this.crit;
+    }
+
+    createDie() {
+        return new Promise((resolve, reject) => {
+            const loader = new GLTFLoader();
+            loader.load('/dice/d20.glb', function (gltf) {
+                resolve(gltf.scene);
+            }, undefined, function (error) {
+                reject(error);
+            });
+        });
     }
 }
 
